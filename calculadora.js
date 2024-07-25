d579dc2af796fbb41e65218998e73e940e71427e
antoniofmoliveira <antoniofmoliveia@outlook.com>
2024-07-25 17:55:58 -0300

    fix: add jsdcos and comments

--- calculadora.js
+++ calculadora.js
@@ -1,126 +1,183 @@
 /**
- * controls if display should be cleaned before new entry
+ * Flag indicating whether the '=' button has been clicked.
+ * This flag is used to determine if the 'C' button should clear the display before
+ * updating it with the result of the calculation.
+ * @type {boolean}
  */
 let equal_typed = false;
 
 /**
- * handles clicks on most keys of calculator
- * @param {*} value the value of the key
+ * Handles a click on the calculator buttons.
+ * @param {string} value - Value of the button that was clicked.
  */
 const onclick_handle = (value) => {
-    if (equal_typed) {
-        onclick_c();
-        equal_typed = false;
-    }
-    let element = document.getElementById("display");
-    let disp_text = element.innerText.trim();
-    let is_dup_op =
-        (disp_text.endsWith("**") ||
-            disp_text.endsWith("/") ||
-            disp_text.endsWith("+") ||
-            disp_text.endsWith("-")) &&
-        (value === "*" || value === "/" || value === "+" || value === "-");
-    let is_first_not_valid =
-        disp_text.trim() === "" && (value === "*" || value === "/");
-    if (
-        is_dup_op ||
-        is_first_not_valid ||
-        (value === "." && element.innerText.indexOf(".") > -1) ||
-        (value === "0" && element.innerText === "0")
-    ) {
-        return;
-    }
-    element.innerText += value;
-    try {
-        eval(document.getElementById("display").innerText);
-        document.getElementById("alerta").style.backgroundColor = "white";
-    } catch {
-        document.getElementById("alerta").style.backgroundColor = "red";
-    }
+  // If the '=' button was previously clicked, clear the display.
+  if (equal_typed) {
+    onclick_c();
+    equal_typed = false;
+  }
+
+  const element = document.getElementById("display");
+  const disp_text = element.innerText.trim();
+
+  // Do not allow two operators in a row.
+  const is_dup_op =
+    (disp_text.endsWith("**") ||
+      disp_text.endsWith("/") ||
+      disp_text.endsWith("+") ||
+      disp_text.endsWith("-")) &&
+    (value === "*" || value === "/" || value === "+" || value === "-");
+
+  // Do not allow '*' or '/' as the first character.
+  const is_first_not_valid =
+    disp_text.trim() === "" && (value === "*" || value === "/");
+
+  // Do not allow more than one '.'.
+  const has_dot = element.innerText.indexOf(".") > -1;
+
+  // Do not allow '0' as the first character unless it is part of '0.'
+  const is_first_zero = disp_text.trim() === "0" && value !== ".";
+
+  // If any of the above conditions are true, do not allow the character.
+  if (is_dup_op || is_first_not_valid || has_dot || is_first_zero) {
+    return;
+  }
+
+  element.innerText += value;
+
+  // Evaluate the expression in the display and update the alert box.
+  try {
+    eval(document.getElementById("display").innerText);
+    document.getElementById("alerta").style.backgroundColor = "white";
+  } catch {
+    document.getElementById("alerta").style.backgroundColor = "red";
+  }
 };
 
 /**
- * handles clicks on CE key cleaning the last number or operation entered
+ * Handles a click on the 'CE' button.
+ * This function is called whenever the user clicks the 'CE' button.
+ * It removes the last character from the display and evaluates the expression
+ * in the display. If the expression is valid, the background of the alert box
+ * is set to white. If the expression is invalid, the background of the alert box
+ * is set to red.
  */
 const onclick_ce = () => {
-    document.getElementById("display").innerText = document
-        .getElementById("display")
-        .innerText.slice(0, -1);
-    try {
-        eval(document.getElementById("display").innerText);
-        document.getElementById("alerta").style.backgroundColor = "white";
-    } catch {
-        document.getElementById("alerta").style.backgroundColor = "red";
-    }
+  // Remove the last character from the display.
+  document.getElementById("display").innerText = document
+    .getElementById("display")
+    .innerText.slice(0, -1);
+
+  // Evaluate the expression in the display.
+  try {
+    eval(document.getElementById("display").innerText);
+
+    // If the expression is valid, set the background of the alert box to white.
+    document.getElementById("alerta").style.backgroundColor = "white";
+  } catch {
+    // If the expression is invalid, set the background of the alert box to red.
+    document.getElementById("alerta").style.backgroundColor = "red";
+  }
 };
 
 /**
- * handles clicks on C key cleaning the display
+ * Handles a click on the 'C' button.
+ * This function is called whenever the user clicks the 'C' button.
+ * It clears the display.
  */
 const onclick_c = () => {
-    document.getElementById("display").innerText = "";
+  // Clear the display.
+  document.getElementById("display").innerText = "";
 };
 
 /**
- * handles click on equal key
- * if the argument entered in display cannot be evaled does nothing
+ * Handles a click on the '=' button.
+ * This function is called whenever the user clicks the '=' button.
+ * It evaluates the expression in the display and updates the display with the result.
+ * If the expression is valid, the background of the alert box is set to white.
+ * If the expression is invalid, the background of the alert box is set to red.
  */
 const onclick_equal = () => {
-    try {
-        const result = eval(document.getElementById("display").innerText);
-        move_history(3);
-        move_history(2);
-        move_history(1);
-        document.getElementById("time1").innerText = get_formated_date(
-            new Date()
-        );
-        document.getElementById("hist1").innerText =
-            document.getElementById("display").innerText;
-        document.getElementById("display").innerText = result;
-        equal_typed = true;
-    } catch {}
+  try {
+    // Evaluate the expression in the display.
+    const result = eval(document.getElementById("display").innerText);
+
+    // Move the last three operations to the history.
+    move_history(3);
+    move_history(2);
+    move_history(1);
+
+    // Set the first history item to the current date and the expression in the display.
+    document.getElementById("time1").innerText = get_formated_date(new Date());
+    document.getElementById("hist1").innerText =
+      document.getElementById("display").innerText;
+
+    // Set the display to the result of the expression.
+    document.getElementById("display").innerText = result;
+
+    // Set the flag indicating that the '=' button has been clicked.
+    equal_typed = true;
+  } catch {
+    // If the expression is invalid, the background of the alert box is set to red.
+    document.getElementById("alerta").style.backgroundColor = "red";
+  }
 };
 
 /**
- * formats a date in pt-BR format DD/MM/YYYY HH:MM:SS
- * @param {*} date a date
- * @returns  date in pt-BR format DD/MM/YYYY HH:MM:SS
+ * Formats a date object into a string in the format "DD/MM/YYYY HH:MM:SS".
+ *
+ * @param {Date} date - The date object to be formatted.
+ * @returns {string} The formatted date string.
  */
 const get_formated_date = (date) => {
-    const day = `0${date.getDate()}`.slice(-2);
-    const month = `0${date.getMonth() + 1}`.slice(-2);
-    const hours = `0${date.getHours()}`.slice(-2);
-    const minutes = `0${date.getMinutes()}`.slice(-2);
-    const seconds = `0${date.getSeconds()}`.slice(-2);
-    return `${day}/${month}/${date.getFullYear()} ${hours}:${minutes}:${seconds}`;
+  // Pad the day, month, hours, minutes, and seconds with leading zeros
+  const day = `0${date.getDate()}`.slice(-2);
+  const month = `0${date.getMonth() + 1}`.slice(-2);
+  const hours = `0${date.getHours()}`.slice(-2);
+  const minutes = `0${date.getMinutes()}`.slice(-2);
+  const seconds = `0${date.getSeconds()}`.slice(-2);
+
+  // Concatenate the formatted date and time into a single string
+  return `${day}/${month}/${date.getFullYear()} ${hours}:${minutes}:${seconds}`;
 };
 
 /**
- * moves a history entry to a higher position
- * there are four positions in history
- * @param {*} origin
+ * Moves the content of a history item to the next item.
+ *
+ * @param {number} origin - The index of the history item to move.
  */
 const move_history = (origin) => {
-    const destination = origin + 1;
-    const time_origin = document.getElementById("time" + origin);
-    const hist_origin = document.getElementById("hist" + origin);
-    const time_destination = document.getElementById("time" + destination);
-    const hist_destination = document.getElementById("hist" + destination);
-    hist_destination.innerText = hist_origin.innerText;
-    time_destination.innerText = time_origin.innerText;
+  // Calculate the index of the destination history item.
+  const destination = origin + 1;
+
+  // Get the necessary DOM elements.
+  const time_origin = document.getElementById("time" + origin);
+  const hist_origin = document.getElementById("hist" + origin);
+  const time_destination = document.getElementById("time" + destination);
+  const hist_destination = document.getElementById("hist" + destination);
+
+  // Move the content of the origin history item to the next item.
+  hist_destination.innerText = hist_origin.innerText;
+  time_destination.innerText = time_origin.innerText;
 };
 
 /**
- * handles clicks in history element filling the display with it
- * resets equal_typed flag
- * @param {*} origin
- * @returns
+ * Recovers the content of a history item and sets it as the current display value.
+ *
+ * @param {number} origin - The index of the history item to recover.
  */
 const recover_history = (origin) => {
-    const recovered = document.getElementById("hist" + origin).innerText.trim();
-    if (recovered.length <= 0) return;
-    document.getElementById("display").innerText = document.getElementById(
-        "hist" + origin
-    ).innerText;
-    equal_typed = false;
+  // Get the content of the history item and trim any leading/trailing whitespace
+  const recovered = document.getElementById("hist" + origin).innerText.trim();
+
+  // If the history item is empty, do nothing
+  if (recovered.length <= 0) return;
+
+  // Set the current display value to the recovered history item
+  document.getElementById("display").innerText = document.getElementById(
+    "hist" + origin
+  ).innerText;
+
+  // Reset the flag indicating that the '=' button has been clicked
+  equal_typed = false;
 };
